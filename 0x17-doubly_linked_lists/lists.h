e07d12fa20df8737bb6c17788a888993897fdadf
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-09-29 19:54:37 -0700

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
-#include <stddef.h>
+#ifndef LISTS_H
+#define LISTS_H
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -21,12 +23,12 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* _LISTS_H_ */
+#endif
