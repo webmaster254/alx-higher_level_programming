ce89340c5ff292de8732f1c86c1e48e8a7317706
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-12-19 11:44:14 -0800

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-const secondBiggest = (...numbers) => {
-  // If no argument passed or only one argument passed, print 0
-  if (numbers.length <= 1) {
-    console.log(0);
-    return;
-  }
-
-  // Sort the numbers in descending order
-  numbers.sort((a, b) => b - a);
-
-  // The second biggest number is the second element in the sorted array
-  console.log(numbers[1]);
+let nums = process.argv.slice(2).map(x => Number(x));
+if (nums.length <= 1) {
+  console.log(0);
+} else {
+  console.log(nums.sort().reverse()[1]);
 }
