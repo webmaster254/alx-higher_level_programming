2a0abf0564850b61f8961e1a0b3a5ad0aec3acd1
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-12-19 10:47:46 -0800

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const firstArgument = process.argv[2];
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(Number(firstArgument)));
