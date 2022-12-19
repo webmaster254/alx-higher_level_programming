59ec5c4790e405c47f8d296f9f521244bcc2eaa7
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-12-19 10:44:40 -0800

    add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const firstArgument = process.argv[2];
+const secondArgument = process.argv[3];
+
+function add(a, b) {
+  return a + b;
+}
+
+if (Number.isInteger(Number(firstArgument)) && Number.isInteger(Number(secondArgument))) {
+  console.log(add(Number(firstArgument), Number(secondArgument)));
+} else {
+  console.log("One or both arguments are not integers");
+}
