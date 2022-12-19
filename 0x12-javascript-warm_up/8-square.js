b4665f82d8541d82b977ffbfe480fd791742af95
Joseph Macharia <37827513+webmaster254@users.noreply.github.com>
2022-12-19 10:43:49 -0800

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const size = process.argv[2];
+
+if (Number.isInteger(Number(size))) {
+  for (let i = 0; i < Number(size); i++) {
+    let row = "";
+    for (let j = 0; j < Number(size); j++) {
+      row += "X";
+    }
+    console.log(row);
+  }
+} else {
+  console.log("Missing size");
+}
